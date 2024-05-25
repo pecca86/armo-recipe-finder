f6d5ed2791cdd83d00cc29bf1effce330a3eac35
Pekka Ranta-aho <pekka.ranta-aho@signify.com>
2024-05-25 20:53:41 +0200

    - Added filtering to all recipes request

--- recipe-finder/src/main/java/org/recipefinder/recipefinder/recipe/RecipeRepository.java
+++
@@ -1,8 +0,0 @@
-package org.recipefinder.recipefinder.recipe;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-}
