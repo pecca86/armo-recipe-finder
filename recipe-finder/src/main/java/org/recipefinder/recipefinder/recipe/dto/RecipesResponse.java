7cd8fdd33cc57f3d0bff62bb84ced62338522eaf
Pekka Ranta-aho <pekka.ranta-aho@signify.com>
2024-05-26 14:05:14 +0200

    - Delete unused classes.

--- recipe-finder/src/main/java/org/recipefinder/recipefinder/recipe/dto/RecipesResponse.java
+++
@@ -1,8 +0,0 @@
-package org.recipefinder.recipefinder.recipe.dto;
-
-import org.recipefinder.recipefinder.recipe.Recipe;
-
-import java.util.List;
-
-public record RecipesResponse(int StatusCode, String message, List<Recipe> payload) {
-}
