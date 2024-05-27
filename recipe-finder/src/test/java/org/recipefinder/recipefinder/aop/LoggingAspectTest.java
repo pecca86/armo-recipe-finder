909267f1d76011051503c216d5313fc1c6815d85
Pekka Ranta-aho <pekka.ranta-aho@signify.com>
2024-05-27 23:52:43 +0200

    - added tests for AOP

---
+++ recipe-finder/src/test/java/org/recipefinder/recipefinder/aop/LoggingAspectTest.java
@@ -0,0 +1,62 @@
+package org.recipefinder.recipefinder.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.recipefinder.recipefinder.auth.dto.RegisterRequest;
+
+import org.slf4j.Logger;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class LoggingAspectTest {
+
+    @Mock
+    private ProceedingJoinPoint proceedingJoinPoint;
+
+    @InjectMocks
+    private LoggingAspect underTest;
+
+    @Mock
+    private Logger LOGGER;
+
+    @Test
+    void should_produce_logging_when_user_registers() throws Throwable {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = System.out;
+        System.setOut(new PrintStream(baos));
+
+        RegisterRequest r = new RegisterRequest("name", "lastname", "email", "pw");
+        Object[] args = new Object[] {r};
+        when(proceedingJoinPoint.getArgs()).thenReturn(args);
+        underTest.logRegister(proceedingJoinPoint);
+        System.setOut(ps);
+        String logOutput = baos.toString();
+        assertThat(logOutput).contains("Logging register")
+                             .contains("Register request: email");
+    }
+
+    @Test
+    void should_produce_logging_when_costumer_performs_login() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = System.out;
+        System.setOut(new PrintStream(baos));
+
+        RegisterRequest r = new RegisterRequest("name", "lastname", "email", "pw");
+        Object[] args = new Object[] {r};
+        when(proceedingJoinPoint.getArgs()).thenReturn(args);
+        underTest.logLogin(proceedingJoinPoint);
+        System.setOut(ps);
+        String logOutput = baos.toString();
+        assertThat(logOutput).contains("Logging login")
+                             .contains("Login attempt for user: email");
+    }
+}
